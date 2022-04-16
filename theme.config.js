16e1fa3f877ccaf7874972fa52d725ba584ace7f
MiniNaim <mininaim@gmail.com>
2022-04-16 01:21:34 +0000

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -4,7 +4,7 @@ export default {
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
       <time>{YEAR}</time> © MiniNaim.
-      <a href="/feed.xml">RSS</a>
+
       <style jsx>{`
         a {
           float: right;
